1fa91f95f52a7fa98479c815ffa6326739d1b64d
Bobby Cochran <bobby.cochran@dgnatech.com>
2019-07-21 16:04:33 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>There is prod.</p>
+        <p>There is feature-branch-start.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
